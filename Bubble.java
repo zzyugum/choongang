680dcdb7a587a0487845152f3f15234fd18242e5
zzyugum <120879289+zzyugum@users.noreply.github.com>
2023-05-25 21:38:56 +0900

    2023.05.25

---
+++ Bubble.java
@@ -0,0 +1,7 @@
+package static0525;
+
+public class Bubble {
+	
+	public static int[] arr = null;
+
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+package static0525;
+
+public class Test {
+
+	public static int[] arr = {0,0,0};
+	public static String aaa() {
+		return "홍길동";
+	}
+}
