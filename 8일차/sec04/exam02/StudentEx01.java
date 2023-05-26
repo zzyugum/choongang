81568c4e7d4b8ce387c10e63b0640813396d0afc
zzyugum <120879289+zzyugum@users.noreply.github.com>
2023-05-26 16:27:55 +0900

    Add files via upload

---
+++ 8일차/sec04/exam02/StudentEx01.java
@@ -0,0 +1,23 @@
+package sec04.exam02;
+
+
+
+public class StudentEx01 {
+
+	public static void main(String[] args) {
+		
+		Student stu = new Student("홍길동", 90, 80, 90);
+		
+		stu.printStudent();
+		
+
+	}
+
+}
+
+
+
+
+// while(run){
+//	}
+
