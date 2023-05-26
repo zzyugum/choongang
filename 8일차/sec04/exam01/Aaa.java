6c64a606133f7274b8c5119a151d939b643ca236
zzyugum <120879289+zzyugum@users.noreply.github.com>
2023-05-26 16:25:32 +0900

    Create Aaa.java

---
+++ 8일차/sec04/exam01/Aaa.java
@@ -0,0 +1,17 @@
+package sec04.exam01;
+
+public class Aaa {
+
+	String name;
+	//생성 되는 객처의 특징
+	
+	String getName() {
+		return name;
+	}
+	void go() {
+		
+		System.out.println(name+"간다");
+		
+	}
+
+}
