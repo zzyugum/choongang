0faac35ad056fffb6347ef7422744499f51f0a97
zzyugum <120879289+zzyugum@users.noreply.github.com>
2023-06-01 22:23:57 +0900

    Add files via upload

---
+++ 12일차/get0601/sec06/exam06/CarExample.java
@@ -0,0 +1,21 @@
+package sec06.exam06;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		
+		myCar.setSpeed(-50);
+		
+		System.out.println("현재 속도: " +myCar.getSpeed());
+
+		myCar.setSpeed(60);
+		
+		if(!myCar.isStop()) {
+			myCar.setStop(true);
+		}
+		
+		System.out.println("현재 속도: " + myCar.getSpeed());
+	}
+
+}
